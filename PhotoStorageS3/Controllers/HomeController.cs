375d56cef5a5f8d9e3b2771e4f4b258055e0181b
JOVIANLIM.17@ichat.sp.edu.sg <JOVIANLIM.17@ichat.sp.edu.sg>
2020-06-26 19:57:10 +0800

    Add project files.

---
+++ PhotoStorageS3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoStorageS3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            S3Uploader amazonS3 = new S3Uploader();
+
+            amazonS3.UploadFile();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PhotoStorageS3/S3Uploader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace PhotoStorageS3
+{
+    public class S3Uploader
+    {
+        private string bucketName = "photostorageassignment";
+        private string keyName = "testTalentsPhotoUpload";
+        private string filePath = "C:\\Users\\DKroz\\Desktop\\School\\CSC\\Talents photo\\Barot_Bellingham_tn.jpg";
+
+        public void UploadFile()
+        {
+            var client = new AmazonS3Client(Amazon.RegionEndpoint.APSoutheast1);
+
+            try
+            {
+                PutObjectRequest putRequest = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    FilePath = filePath,
+                    ContentType = "text/plain"
+                };
+
+                PutObjectResponse response = client.PutObject(putRequest);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    throw new Exception("Check the provided AWS Credentials.");
+                }
+                else
+                {
+                    throw new Exception("Error occurred: " + amazonS3Exception.Message);
+                }
+            }
+        }
+    }
+}
